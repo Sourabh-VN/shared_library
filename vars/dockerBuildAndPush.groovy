c338428f62b67fa9e84bc66e56ed11c690add65f
Sourabh-VN <sourabhv2407@gmail.com>
2024-07-09 17:58:54 +0530

    gave destination path for build

--- vars/dockerBuildAndPush.groovy
+++ vars/dockerBuildAndPush.groovy
@@ -1,7 +1,7 @@
 def call(String dockerImage, String dockerImageTag, String dockerFilePath, String registryCredentialsid){
     def imageName= "${dockerImage}:${dockerImageTag}"
     echo "IMAGE:${imageName}"
-    def buildImage= docker.build(imageName,"-f ${dockerFilePath}")
+    def buildImage= docker.build(imageName,"-f ${dockerFilePath} .")
     docker.withRegistry('', registryCredentialsid){
         buildImage.push()
     }
