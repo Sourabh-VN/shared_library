8b8750020d74d0ff151b684f4f6cd3db154918e8
Sourabh-VN <sourabhv2407@gmail.com>
2024-07-09 14:32:51 +0530

    {}brackets added

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,4 +1,5 @@
-def call(Map config)
+def call(Map config) {
     checkout($class: 'GitSCM',
                                 branches: [[name: config.branch]],
                                 userRemoteConfigs: [[url: config.url, credentialsId: config.credId]])
+                                }
